cf8d04343cfad67bbee239290e0920e7bdf93419
Jana Nisavic <nj200317d@student.etf.bg.ac.rs>
2023-07-02 13:49:14 +0200

    Implementirano sortiranje umetnina

--- src/views/ArtWorks.vue
+++ src/views/ArtWorks.vue
@@ -1,6 +1,22 @@
 <template>
+
+	<div class="d-flex">
+		<div class="dropdown ms-auto mt-1 mb-3 me-3">
+			<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+								Сортирање
+			</a>
+		<ul class="dropdown-menu">
+			<li><button class="dropdown-item" type="button" @click="currentArtwork = artworksByArtWorkNameUp">Назив уметнине растуће</button></li>
+			<li><button class="dropdown-item" type="button" @click="currentArtwork = artworksByArtWorkNameDown">Назив уметнине опадајуће</button></li>
+			<li><button class="dropdown-item" type="button" @click="currentArtwork = artworksByArtistsNameUp">Назив уметника растуће</button></li>
+			<li><button class="dropdown-item" type="button" @click="currentArtwork = artworksByArtistsNameDown">Назив уметника опадајуће</button></li>
+		</ul>
+		</div>
+	</div>
+	
+
 	<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
-		<Artwork v-for="artwork in categoryArtworks" :key="artwork.title" :artwork="artwork" />
+		<Artwork v-for="artwork in currentArtwork" :key="artwork.title" :artwork="artwork" />
 	</div>
 </template>
 
@@ -13,6 +29,14 @@ export default {
 	components: {
 		Artwork
 	},
+	created(){
+		this.currentArtwork = this.categoryArtworks;
+	},
+	data() {
+		return{
+			currentArtwork:this.categoryArtworks
+		}
+	}, 
 	computed: {
 		allArtworks() {
 			return artworks;
@@ -20,6 +44,31 @@ export default {
 		categoryArtworks() {
 			return artworks.filter(artwork => artwork.category === this.$route.params.category);
 		},
+		artworksByArtistsNameUp() {
+			return artworks.sort(cmpFunctionByAristsUp).filter(artwork => artwork.category === this.$route.params.category);
+		},
+		artworksByArtistsNameDown() {
+			return artworks.sort(cmpFunctionByAristsDown).filter(artwork => artwork.category === this.$route.params.category);
+		},
+		artworksByArtWorkNameUp() {
+			return artworks.sort(cmpFunctionByArtWorkUp).filter(artwork => artwork.category === this.$route.params.category);
+		},
+		artworksByArtWorkNameDown() {
+			return artworks.sort(cmpFunctionByArtWorkDown).filter(artwork => artwork.category === this.$route.params.category);
+		}
 	}
+}
+
+function cmpFunctionByAristsUp(first, second){
+		return first.artist <= second.artist ? -1 : 1;
+}
+function cmpFunctionByAristsDown(first, second){
+		return first.artist >= second.artist ? -1 : 1;
+}
+function cmpFunctionByArtWorkUp(first, second){
+		return first.title <= second.title ? -1 : 1;
+}
+function cmpFunctionByArtWorkDown(first, second){
+		return first.title >= second.title ? -1 : 1;
 }
 </script>
